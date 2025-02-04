082b60eec1fad8e72200d9fc881d216eb5112c5a
Anubhab <anubhabpal1@gmail.com>
2025-02-05 00:55:08 +0530

    fix navbar

--- src/components/NavLink.jsx
+++ src/components/NavLink.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
 const NavLink = ({ title, href, onClick, isMobile }) => {
-  return <a className={`group relative px-4${isMobile ? 'px-3' : "py-2"} rounded-xl text-gray-700 font-medium transition-colors duration-200`} onClick={onClick} >
-    <span>{title}</span>
+  return <a href={href} className={`group relative px-4 ${isMobile ? 'px-3' : 'py-2'} rounded-xl text-gray-700 font-medium transition-colors duration-200 cursor-pointer`} onClick={onClick} >
+    <span className="relative z-10 group-hover:text-red-500 transition-colors duration-300">{title}</span>
 
-    {!isMobile && <div></div>}
+    {!isMobile && <div className="absolute inset-0 bg-red-50 rounded-xl scale-90 opacity-0 group-hover:opacity-100 group-hover:scale-100 translate-all duration-300"></div>}
   </a>
 };
 
--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Logo from '../components/Logo';
 import IconButton from '../components/IconButton';
 import NavLink from '../components/NavLink';
@@ -6,6 +6,16 @@ import { Menu, ShoppingCart, User, X } from 'lucide-react';
 
 const Header = () => {
     const [isOpen, setIsOpen]= useState(false);
+    const [scrolled, setScrolled] = useState(false);
+
+    useEffect(()=>{
+        const handleScroll = ()=> setScrolled(window.scrollY>20);
+        window.addEventListener('scroll', handleScroll);
+
+        return ()=> window.removeEventListener("scroll", handleScroll);
+    }, []);
+
+
     const navLinks =[
         {title: "Home", href: "#"},
         {title: "Menu", href: "#"},
@@ -14,32 +24,32 @@ const Header = () => {
         {title: "Rewards", href: "#"}
     ]
   return (
-    <header className={`fixed w-full top-0 z-50 transition-all duration-300`}>
+    <header className={`fixed w-full top-0 z-50 transition-all duration-300 ${scrolled ? "py-4 bg-white/80 backdrop-blur-lg shadow-[0_8px_30px_rgb(0,0,0.12)]" : "py-6 bg-transparent"}`}>
         <div className="max-w-7xl mx-auto px-4">
             <div className="flex items-center justify-between">
                 <Logo />
 
                 {/* Desktop Navigation */}
                 <div className="hidden lg:flex items-center space-x-1">
-                    {navLinks.map(Link=>(
-                        <NavLink />
+                    {navLinks.map(link=>(
+                        <NavLink key={link.title} {...link}/>
                     ))}
                 </div>
 
                 {/* Right Section */}
                 <div className='flex items-center gap-6'>
                     <IconButton icon={ShoppingCart} count={3} />
-                    <IconButton icon={User} />
+                    <IconButton icon={User} className="hidden md:block" />
                     <IconButton icon={isOpen ? X : Menu} className="lg:hidden" onClick={()=> setIsOpen(!isOpen)} />
                 </div>
             </div>
 
             {/* Mobile Navigation */}
-            <div className={`lg:hidden fixed inset-0 to-[80px] bg-white/80 backdrop-blur-lg transition-all duration-300 ease-in-out ${isOpen ? "translate-x-0" : "translate-x-full"}`}>
+            <div className={`lg:hidden fixed inset-0 top-[80px] bg-white/80 backdrop-blur-lg transition-all duration-300 ease-in-out ${isOpen ? "translate-x-0" : "translate-x-full"}`}>
                 <nav className="bg-white/90 border-t">
                     <div className="max-w-7xl mx-auto px-4 py-2 flex flex-col gap-2">
-                        {navLinks.map((Link) => (
-                            <NavLink />
+                        {navLinks.map((link) => (
+                            <NavLink key={link.title} {...link} isMobile onClick={()=> setIsOpen(false)} />
                         ))}
                     </div>
                 </nav>
